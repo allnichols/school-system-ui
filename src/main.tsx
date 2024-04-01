21bbcc4b3759b59809a94c9bb952f17842ca0065
Max <allenmaxnichols@gmail.com>
2024-04-01 17:30:47 -0500

    fix link styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./main.css";
 
 import { routeTree } from "./routeTree.gen.ts";
 
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,40 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Header from "../components/header";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/teachers" className="[&.active]:font-bold">
-        Teachers
-      </Link>{" "}
-      <Outlet />
-      <TanStackRouterDevtools />
+      <Header>
+        <Button>
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/"
+          >
+            Home
+          </Link>
+        </Button>{" "}
+        <Button>
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/teachers"
+          >
+            Teachers
+          </Link>
+        </Button>{" "}
+        <TanStackRouterDevtools />
+      </Header>
+      <Container style={{ marginTop: "2rem" }} maxWidth="lg">
+        <Outlet />
+      </Container>
     </>
   ),
 });
