46ab6a3a59efc045bf5c966d9c1c5527845a9540
Max <allenmaxnichols@gmail.com>
2024-04-02 17:27:48 -0500

    add single teacher page

--- src/routes/teachers.$id.tsx
+++ src/routes/teachers.$id.tsx
@@ -2,10 +2,5 @@ import { createFileRoute } from "@tanstack/react-router";
 import Teacher from "../views/teachers/teacher";
 
 export const Route = createFileRoute("/teachers/$id")({
-  parseParams: (params) => {
-    return {
-      id: params.id,
-    };
-  },
   component: () => <Teacher />,
 });
--- src/views/teachers/teacher.tsx
+++ src/views/teachers/teacher.tsx
@@ -1,9 +1,21 @@
 import { useParams } from "@tanstack/react-router";
-
+import { useFetchTeacher } from "./query/useFetchTeacher";
 const Teacher = () => {
-  const params = useParams({ from: "/teachers/$id" });
-  console.log(params);
-  return <div>Teacher</div>;
+  const { id } = useParams({ from: "/teachers/$id" });
+  const { loading, error, data } = useFetchTeacher(id);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div>Not found</div>;
+
+  return (
+    <div>
+      <h1>
+        {data.getTeacherById.firstName} {data.getTeacherById.lastName}
+      </h1>
+      <p>{data.getTeacherById.email}</p>
+      <p>{data.getTeacherById.dob}</p>
+    </div>
+  );
 };
 
 export default Teacher;
