8df8f2e6f1ccba66b7c3e3fcd024dc32f17bad5f
Max <allenmaxnichols@gmail.com>
2025-03-14 17:06:26 -0500

    move header add new layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,52 +1,62 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import Header from "../components/header";
+import Sidebar from "../components/sidebar";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
 import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
 import Localization from "../utils/Localization";
+import { Box, ListItemContent, Typography } from "@mui/joy";
+
+const SidebarItems = [
+  {
+    title: "Home",
+    to: "/",
+  },
+  {
+    title: "Teachers",
+    to: "/teachers",
+  },
+  {
+    title: "Courses",
+    to: "/courses",
+  },
+];
 
 export const Route = createRootRoute({
   component: () => (
     <Localization>
-      <Header>
-        <Button>
-          <Link
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-            to="/"
-          >
-            Home
-          </Link>
-        </Button>{" "}
-        <Button>
-          <Link
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-            to="/teachers"
-          >
-            Teachers
-          </Link>
-        </Button>{" "}
-        <Button>
-          <Link
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-            to="/courses"
-          >
-            Courses
-          </Link>
-        </Button>{" "}
-        <TanStackRouterDevtools />
-      </Header>
-      <Container style={{ marginTop: "2rem" }} maxWidth="lg">
-        <Outlet />
-      </Container>
+      <Box sx={{ display: "flex", height: "100%" }}>
+        <Sidebar>
+          <List>
+            {SidebarItems.map((item) => (
+              <ListItem key={item.title}>
+                <ListItemContent>
+                  <Typography level="title-sm">{item.title}</Typography>
+                </ListItemContent>
+                {/* <Link
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                }}
+                to={item.to}
+              >
+                {item.title}
+              </Link> */}
+              </ListItem>
+            ))}
+          </List>
+          <TanStackRouterDevtools />
+        </Sidebar>
+        <Box
+          sx={{
+            flexGrow: 1, // Take up the remaining space
+            padding: 2, // Add some padding
+            overflow: "auto", // Enable scrolling if content overflows
+          }}
+        >
+          <Outlet />{" "}
+        </Box>
+      </Box>
     </Localization>
   ),
 });
