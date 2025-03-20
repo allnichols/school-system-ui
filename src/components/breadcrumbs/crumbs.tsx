ec9bd8b49318adf753a1e685368172c866c97765
Max <allenmaxnichols@gmail.com>
2025-03-20 15:51:11 -0500

    add spacing for bread crumbs

--- src/components/breadcrumbs/crumbs.tsx
+++ src/components/breadcrumbs/crumbs.tsx
@@ -4,20 +4,24 @@ function Crumbs() {
   const router = useRouterState();
   const location = router.location.pathname.split("/");
   const pathnames = location.filter((path) => path !== "");
-  console.log(pathnames);
+
   return (
     <>
-      <Link to="/" style={{ textDecoration: "none" }}>
+      <Link to="/" style={{ textDecoration: "none", marginRight: "4px" }}>
         Home
       </Link>
       {pathnames.map((path, index) => {
         const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
         const isLast = index === pathnames.length - 1;
         return isLast ? (
-          <span key={path}>{path}</span>
+          <span key={path}>{" / " + path}</span>
         ) : (
-          <Link key={path} to={routeTo} style={{ textDecoration: "none" }}>
-            {"/ " + path}
+          <Link
+            key={path}
+            to={routeTo}
+            style={{ textDecoration: "none", marginRight: "4px" }}
+          >
+            {" / " + path}
           </Link>
         );
       })}
