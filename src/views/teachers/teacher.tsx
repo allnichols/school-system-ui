a8cd8e5233e9f257ad1f4ee0139d63567e01a235
Max <allenmaxnichols@gmail.com>
2025-04-02 13:02:08 -0500

    add snackbar

--- src/views/teachers/teacher.tsx
+++ src/views/teachers/teacher.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useParams } from "@tanstack/react-router";
 import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import Card from "@mui/joy/Card";
@@ -6,9 +7,14 @@ import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
 import Stack from "@mui/joy/Stack";
 import { Box, Button, Typography } from "@mui/joy";
-import React from "react";
+import SnackbarMain, { Severity } from "../../components/snackbar";
 
 const Teacher = () => {
+  const [openSnackbar, setOpenSnackbar] = React.useState({
+    open: false,
+    message: "",
+    severity: "success",
+  });
   const { id } = useParams({ from: "/teachers/$id" });
 
   const { data, isLoading } = useSuspenseQuery({
@@ -37,6 +43,14 @@ const Teacher = () => {
       }
       return await response.json();
     },
+    onSuccess: () => {
+      console.log("Teacher updated successfully");
+      setOpenSnackbar({
+        open: true,
+        message: `🎉 Teacher ${formData.firstName} ${formData.lastName} updated successfully!`,
+        severity: "success",
+      });
+    },
   });
 
   const [formData, setFormData] = React.useState({
@@ -46,60 +60,97 @@ const Teacher = () => {
     dob: data.dob || "",
   });
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Box
-      component="main"
-      className="MainContent"
-      sx={{
-        pt: { xs: "calc(12px + var(--Header-height))", md: 3 },
-        pb: { xs: 2, sm: 2, md: 3 },
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-        minWidth: 0,
-        height: "100dvh",
-        gap: 1,
-        overflow: "auto",
-      }}
-    >
-      <Typography level="h1">Teacher</Typography>
-      <Card sx={{ padding: 3, marginTop: 2 }}>
-        <FormControl>
-          <FormLabel>First Name</FormLabel>
-          <Input value={formData.firstName} sx={{ marginBottom: 2 }} />
-        </FormControl>
+    <>
+      <Box
+        component="main"
+        className="MainContent"
+        sx={{
+          pt: { xs: "calc(12px + var(--Header-height))", md: 3 },
+          pb: { xs: 2, sm: 2, md: 3 },
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          minWidth: 0,
+          height: "100dvh",
+          gap: 1,
+          overflow: "auto",
+        }}
+      >
+        <Typography level="h1">Teacher</Typography>
+        <Card sx={{ padding: 3, marginTop: 2 }}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input
+              name="firstName"
+              value={formData.firstName}
+              sx={{ marginBottom: 2 }}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-        <FormControl>
-          <FormLabel>Last Name</FormLabel>
-          <Input value={formData.lastName} sx={{ marginBottom: 2 }} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input value={formData.email} sx={{ marginBottom: 2 }} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Date of Birth</FormLabel>
-          <Input value={formData.dob} sx={{ marginBottom: 2 }} />
-        </FormControl>
-        <Stack direction="row" spacing={2}>
-          <Button
-            variant="solid"
-            onClick={() => {
-              mutateTeacher.mutate(formData);
-            }}
-          >
-            Save
-          </Button>
-          <Button color="danger" variant="outlined">
-            Delete
-          </Button>
-        </Stack>
-      </Card>
-    </Box>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input
+              name="lastName"
+              value={formData.lastName}
+              sx={{ marginBottom: 2 }}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input
+              name="email"
+              value={formData.email}
+              sx={{ marginBottom: 2 }}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Date of Birth</FormLabel>
+            <Input
+              name="dob"
+              value={formData.dob}
+              sx={{ marginBottom: 2 }}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="solid"
+              onClick={() => {
+                mutateTeacher.mutate(formData);
+              }}
+              loading={mutateTeacher.isPending}
+            >
+              Save
+            </Button>
+            <Button color="danger" variant="outlined">
+              Delete
+            </Button>
+          </Stack>
+        </Card>
+      </Box>
+      <SnackbarMain
+        open={openSnackbar.open}
+        message={openSnackbar.message}
+        severity={openSnackbar.severity as Severity}
+      />
+    </>
   );
 };
 
